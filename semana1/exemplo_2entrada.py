4aed6a8be10497a2a79326255160280d74e37c89
Nathan Couto <nathancouto251@gmail.com>
2024-02-19 10:14:44 -0300

    Aulas da semana 1

---
+++ semana1/exemplo_2entrada.py
@@ -0,0 +1,5 @@
+#entrada
+var1 = input('Digite o número 1: ')
+var2 = input('Digite o número 2: ')
+#saída
+print('Você digitou {} e {}'.format(var1,var2))
---
+++ semana1/exemplo_3processamento.py
@@ -0,0 +1,11 @@
+#entrada
+var1 = int(input('Digite o número 1: '))
+var2 = int(input('Digite o número 2: '))
+#processamento
+soma = var1 + var2
+#saída
+print('A soma é de',var1,' + ',var2,' = ',soma)
+
+# int('10') -> converte o texto '10' para inteiro '10'
+# float('10.5') -> converte o texto '10.5' para inteiro '10'.
+#str(10) -> converte o inteiro '10' para o texto '10'
